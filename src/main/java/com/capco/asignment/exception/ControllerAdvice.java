347d67ce09ae3bfb1dacdc04379eafba27b45b3c
andrzej.salamon@gmail.com <andrzej.salamon@gmail.com>
2023-01-23 09:45:54 +0100

    initial commit

---
+++ src/main/java/com/capco/asignment/exception/ControllerAdvice.java
@@ -0,0 +1,37 @@
+package com.capco.asignment.exception;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+/**
+ * Class to handle exceptions thrown by controller
+ */
+@RestControllerAdvice
+public class ControllerAdvice {
+    @ExceptionHandler(value = NoSuchElementException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public void featureNotFound() {}
+    @ExceptionHandler(value = ConstraintViolationException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public List<String> wrongInputPathVariableRequestParam(ConstraintViolationException exception) {
+        return exception.getConstraintViolations()
+                .stream()
+                .map(constraintViolation -> constraintViolation.getMessage())
+                .toList();
+    }
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public List<String> wrongInputRequestBody(MethodArgumentNotValidException exception) {
+        return exception.getBindingResult().getFieldErrors()
+                .stream()
+                .map(error -> error.getDefaultMessage())
+                .toList();
+    }
+}
